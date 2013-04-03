2b476935dd87f52e3feaf1857e4160e8a94047c1
Loick <loick.ledigabel@gmail.com>
2013-04-03 14:31:49 +0200

    add view

---
+++ js/views/viewProjet.js
@@ -0,0 +1,15 @@
+greg = greg || {};
+
+var greg.projectView = Backbone.View.extend({
+	tagName : 'section',
+	className: 'project',
+
+	initialize : function(){
+		this.listenTo(this.model, "change", this.render);
+	},
+
+	render : function(){
+		this.$el.html(this.template(this.model.toJSON()));
+    	return this;
+	}
+});
