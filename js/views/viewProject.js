cb9b6cf183fa2ebbb2130532c748309b951e9e5d
Loick <loick.ledigabel@gmail.com>
2013-04-03 17:20:04 +0200

    view

--- js/views/viewProject.js
+++ js/views/viewProject.js
@@ -1,34 +1,21 @@
 var greg = greg || {};
 
 greg.projectView = Backbone.View.extend({
-	tagName : 'section',
+	tagName : 'li',
 	className: 'project',
 	template: _.template($.ajax({type:"GET",url:'js/templates/project.htm',async:false}).responseText),
 
 	events: {
-		'click .project-nav-next' : 'loadNextProject'
+		
 	},
 
 	initialize : function(){
-
-		this.collection = new greg.Collection();
-		// this.collection
-
-		_.bindAll(this, 'render', 'loadNextProject');
 		this.listenTo(this.model, "change", this.render);
 	},
 
 	render : function(){
 		this.$el.html(this.template(this.model.toJSON()));
     	return this.$el;
-	},
-
-	loadNextProject: function(){
-		this.$el.append(this.template(this.model.toJSON()));
-		return this;
-	},
-
-	loadProjectDetails: function(){
-
 	}
+
 });
