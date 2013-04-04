7f21cb776cdf7eb874a72a3611a5db31932068bc
Loick <loick.ledigabel@gmail.com>
2013-04-04 09:08:46 +0200

    pull datas

--- js/main.js
+++ js/main.js
@@ -2,8 +2,4 @@ var greg = greg || {};
 
 var myModel = new greg.projectModel();
 
-// var collection = new greg.Collection();
-// collection.add({ title : 'Test'});
-// collection.add({ title : 'Tafiole'});
-
-//this.viewProject = new greg.projectView({ model : this });
+var test = new greg.mainView();
--- js/views/viewMain.js
+++ js/views/viewMain.js
@@ -1,46 +1,28 @@
 var greg = greg || {};
 
 greg.mainView = Backbone.View.extend({
-	el:$('section'),
-	className: 'project',
-
-	// template: _.template($.ajax({type:"GET",url:'js/templates/project.htm',async:false}).responseText),
-
-	// events: {
-	// 	'click .project-nav-next' : 'loadNextProject'
-	// },
+	el : $('ul#projects'),
 
 	initialize : function(){
-		_.bindAll(this, 'render', 'loadNextProject');
-
-
-		// <ul id="projects"></ul>
 
 		this.collection = new greg.Collection();
-		this.collection.bind('add',this.appendProject)
 
-		this.listenTo(this.model, "change", this.render);
 		this.addProject({
-			title:'Glouglou'
+			title : 'Glouglou',
+			subTitle : 'héhéhé'
 		});
 	},
 
-	render : function(){
-		var sefl = this;
-		$(this.el).append($('<ul>').attr('id','projects'));
-	},
-
-
-	addProject: function(project){
+	addProject: function(projectJSON){
 		var project = new greg.projectModel();
-		project.set();
-		this.collection.add(project);
-	},
 
-	appendProject: function(item){
-		var projectView = new greg.projectView({
-			model:myModel
-		});
-		$('ul', this.el).append(projectView.render().el);
+		project.set(projectJSON);
+
+		this.collection.add(project);
+
+		var projectView = new greg.projectView({ model:project });
+		
+		$(this.el).append(projectView.render().el);
+
 	}
 });
